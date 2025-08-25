3c3d74d2c2afa3405dd4389c5936873f03d6b5f3
Marcello Miranda <marcello.usa963@gmail.com>
2025-08-25 02:18:35 -0300

    Primeiro algoritmo em ML

---
+++ frutas.py
@@ -0,0 +1,35 @@
+# %%
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn import tree
+# %%
+frutas = pd.read_excel("data\data/dados_frutas.xlsx")
+frutas
+# %%
+arvore = tree.DecisionTreeClassifier(random_state=42)
+arvore
+# %%
+y = frutas["Fruta"]
+caracteristicas = ["Arredondada","Suculenta","Vermelha","Doce"]
+x = frutas[caracteristicas]
+
+# %%
+# Machiche learning
+arvore.fit(x,y)
+# %%
+arvore.predict([[0,0,0,0]])
+# %%
+plt.figure(dpi=400)
+tree.plot_tree(arvore,feature_names=caracteristicas
+               ,class_names=arvore.classes_,filled=True)
+
+
+
+# %%
+proba = arvore.predict_proba([[1,1,1,1]])[0]
+proba
+# %%
+pd.Series(proba,index=arvore.classes_)
+
+# %%
+
